00bace60d9e60d9cec1c549a8ee41ec363d137b0
Vaibhav <vaibhav.gupta1397@gmail.com>
2022-03-02 13:53:41 +0530

    remove comments

--- api/tasks.py
+++ api/tasks.py
@@ -1,23 +1,14 @@
 import logging
-# import schedule
-# import time
 
 from celery import shared_task
 from main.settings import DEVELOPER_KEY, YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, QUERY_TERM
 
-# from celery.schedules import crontab
-# from celery.decorators import periodic_task
+
 from googleapiclient.discovery import build
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-
-# # Periodic Task that runs every minute fetching videos from youtube data api and populating it in the APIVideos Table.
-# @periodic_task(
-#     run_every=(crontab(minute='*/1')),
-#     name="search_youtube",
-#     ignore_result=True)
 
 @shared_task
 def youtube_search():
